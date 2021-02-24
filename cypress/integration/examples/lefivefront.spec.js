01c388f64f07b000ec806918104e92e544dd1b32
Muramash <alexandrebroquet.cd@gmail.com>
2021-02-24 17:18:50 +0100

    cy scripting for testing purpose

--- cypress/integration/examples/lefivefront.spec.js
+++ cypress/integration/examples/lefivefront.spec.js
@@ -3,26 +3,92 @@
 
 describe("LeFiveFront Testing", () =>{
     beforeEach(() => {
-        cy.visit('http://localhost:3000/main')
+        cy.visit('http://localhost:3000/')
     })
     it("Visit site", function(){
+        cy.get(".filterOpaque").click()
         cy.viewport(2400,1600)
-        cy.wait(2000);
-        // cy.scrollTo("0","500", { easing:"swing" })
-        cy.get(".slick-dots>li").eq(1).click()
-        cy.wait(500)
-        cy.get(".slick-dots>li").eq(0).click()
-        cy.wait(500)
-        cy.get(".slick-dots>li").eq(2).click()
-        cy.wait(500)
-        // cy.viewport(500,877)
-        // cy.wait(3000);
-        // cy.scrollTo("0","500", { easing:"swing" })
-        // cy.viewport("samsung-s10");
-        // cy.wait(3000);
-        cy.viewport("iphone-x");
-        // cy.wait(3000);
-        // cy.scrollTo("0","1000");
+        cy.wait(3000);
+        cy.scrollTo("0","500")
+        cy.get(".sliderContainer")
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(2).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(1).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(0).click({scrollBehavior: false})
+        cy.scrollTo("0","1450")
+        cy.wait(2000)
+        cy.scrollTo("0","2300")
+        cy.wait(2000)
+        cy.scrollTo("0","3300")
+        cy.wait(2000)
+        cy.get(".filterOpaque").click()
+        cy.wait(1000)
+        // cy.get(".burgerButton").trigger("mouseover")
+        // cy.wait(1500)
+        // cy.get(".burgerButton").click()
+        // for(let i = 0; i <= 9; i++){
+        //     cy.get(".nav-item").eq(i).trigger("mouseover")
+        //     cy.wait(500)
+        // }
+
+        cy.get(".filterOpaque").click()
+        cy.viewport(1600,1200)
+        cy.wait(3000);
+        cy.scrollTo("0","500")
+        cy.get(".sliderContainer")
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(2).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(1).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(0).click({scrollBehavior: false})
+        cy.scrollTo("0","1700")
+        cy.wait(2000)
+        cy.scrollTo("0","2500")
+        cy.wait(2000)
+        cy.scrollTo("0","3500")
+        cy.wait(2000)
+
+        cy.get(".filterOpaque").click()
+        cy.viewport(980,830)
+        cy.wait(3000);
+        cy.scrollTo("0","500")
+        cy.get(".sliderContainer")
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(2).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(1).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(0).click({scrollBehavior: false})
+        cy.scrollTo("0","2000")
+        cy.wait(2000)
+        cy.scrollTo("0","3000")
+        cy.wait(2000)
+        cy.scrollTo("0","4300")
+        cy.wait(2000)
+
+        cy.get(".filterOpaque").click()
+        cy.viewport("samsung-s10")
+        cy.wait(3000);
+        cy.scrollTo("0","500")
+        cy.wait(2000)
+        cy.scrollTo("0","1300")
+        cy.get(".sliderContainer")
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(2).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(1).click({scrollBehavior: false})
+        cy.wait(1000)
+        cy.get(".slick-dots>li").eq(0).click({scrollBehavior: false})
+        cy.scrollTo("0","1450")
+        cy.wait(2000)
+        cy.scrollTo("0","2300")
+        cy.wait(2000)
+        cy.scrollTo("0","3300")
+        cy.wait(2000)
+
     })
 })
 
