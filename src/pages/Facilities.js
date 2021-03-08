74b63efdb59da2b14830ebd241fb8c19f2fd1437
Muramash <alexandrebroquet.cd@gmail.com>
2021-03-08 17:02:48 +0100

    facility done, need styling

--- src/pages/Facilities.js
+++ src/pages/Facilities.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import $ from 'jquery';
 
 import './css/Facilities.css'
 import { SecondHeader } from './../components/SecondHeader.js'
 import { MainMenu } from './../components/MainMenu.js'
 import { Footer } from './../components/Footer.js'
-import { UISelect }  from './../components/UISelect.js'
 
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -13,6 +13,17 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
+
+import map from './../assets/img/map.jpg'
+import sacramentoimg from './../assets/img/sacramento_min.jpg'
+
+import calendicon from './../assets/img/cal2-icon.svg'
+import phoneicon from './../assets/img/phone-icon.svg'
+import mailicon from './../assets/img/mail-icon.svg'
+import pinicon from './../assets/img/pin-icon.svg'
+
+import facebook from './../assets/img/facebook_fac.svg'
+import instagram from './../assets/img/instagram_fac.svg'
 
 
 class Facilities extends Component {
@@ -35,35 +46,38 @@ class Facilities extends Component {
 				margin: '10px',
 				minWidth: 120,
 				},
-			facilities: {
-				sacramento: {
+			facilities: [
+				{
 					id: 'sacramento',
 					title:"LE FIVE West Sacramento",
 					desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod purus vitae dapibus iaculis. Proin arcu leo, tincidunt eget tincidunt non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod purus vitae dapibus iaculis.",
-					img:"",
+					img: sacramentoimg,
 					loc:"3940 Seaport Blvd Suite #200 West Sacramento, CA 95691",
+					mail: "contact-west-sacramento@lefive.us",
 					phone:"916-248-5557",
 					calendar:"Business hours"
 				},
-				other: {
+				{
 					id: 'other',
 					title:"LE FIVE Other",
 					desc:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
 					img:"",
 					loc:"3940 Seaport Blvd Suite #200 West Sacramento, CA 95691",
+					mail: "contact-west-other@lefive.us",
 					phone:"916-248-5557",
 					calendar:"Business hours"
 				},
-				foo: {
+				{
 					id: 'foo',
 					title:"LE FIVE Foo",
 					desc:"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system.",
 					img:"",
 					loc:"3940 Seaport Blvd Suite #200 West Sacramento, CA 95691",
+					mail: "contact-west-foo@lefive.us",
 					phone:"916-248-5557",
 					calendar:"Business hours"
 				}
-			}
+			]
 		}
 	  }
 
@@ -72,17 +86,27 @@ class Facilities extends Component {
 		this.state.selectedFac = event.target.value;
 		const facilitiesMap = new Map();
 
-		console.log(this.state.facilities)
-		Object.keys(this.state.facilities).map(i => Object.values(this.state.facilities).map(j => facilitiesMap.set([i,j])))
-		console.log(facilitiesMap);
+		for(const facility of this.state.facilities){
+			if(this.state.selectedFac == facility.id){
+				if(document.getElementById('facility_img')){
+					document.getElementById("facility_img").remove('img');
+				}
+				ReactDOM.render(facility.title, document.getElementById('facility_title'));
+				ReactDOM.render(facility.desc, document.getElementById('facility_desc'));
 
-		// facilities.forEach((facility, key)=>{
-		// 	if(this.state.selectedFac == facility.id){
-		// 		console.log("found")
-		// 	}
-		// 	console.log("key is :", key);
-		// 	console.log("value is :", facility);
-		// 	})
+				let imgFacility = document.createElement("img");
+				imgFacility.src = facility.img;
+				imgFacility.setAttribute("id", "facility_img");
+				let src = document.getElementById("content_facilityimg");
+				src.appendChild(imgFacility);
+
+				ReactDOM.render(facility.loc, document.getElementById('facility_loc'))
+				ReactDOM.render(facility.mail, document.getElementById('facility_phone'))
+				ReactDOM.render(facility.phone, document.getElementById('facility_mail'))
+				ReactDOM.render(facility.calendar, document.getElementById('facility_calendar'))
+			}
+		}
+
 		};
   
 	  handleClose = () => {
@@ -200,7 +224,7 @@ class Facilities extends Component {
 		<div className="container">
 			<div className="row">
 				<div className="col">
-					<div className="selectBox">
+					<div className="p-2 selectBox">
 						<Button className={this.state.button} onClick={this.handleOpen}>
 							Filter
 						</Button>
@@ -224,29 +248,38 @@ class Facilities extends Component {
 				</div>
 			</div>
 
-			<div className="largeSpacer-3">
-				
+
+			<div className="row">
+				<div className="pb-4 col">
+					<h2 className="p-4 facilityTitle" id="facility_title"></h2>
+					<span className="facilityDesc" id="facility_desc"></span>
+				</div>
 			</div>
 
 			<div className="row">
-				<div className="col">
-					{/* {this.facilities.map((value, index) => {
-						return <li key={index}>{value}</li>;
-					})} */}
-					{/* it's : {facilities.get(this.state.selectedFac).title}  */}
-					it's : {facilities.forEach((facility, key)=>{
-						if(this.state.selectedFac == facility.id){
-							console.log("found")
-						}
-						console.log("key is :", key);
-						console.log("value is :", facility);
-						return(
-						<p>{facility.title}</p>
-						)
-					})}	
+				<div className="col-lg-6 col-12">
+					<img src={map} className="facilityMap" id="facility_map"></img>
+				</div>
+				<div className="col-lg-6 col-12">
+					<div className="" id="content_facilityimg">
 
+					</div>
+					<div className="row">
+						<div className="col contentFacility">
+							<span><img src={pinicon}></img> <span className="facilityText" id="facility_loc"></span></span>
+							<span><img src={mailicon}></img> <span className="facilityText" id="facility_phone"></span></span>
+							<span><img src={phoneicon}></img> <span className="facilityText" id="facility_mail"></span></span>
+							<span><img src={calendicon}></img> <span className="facilityText" id="facility_calendar"></span></span>
+						</div>
+						<div className="col">
+							<img src={instagram}></img>
+							<img src={facebook}></img>
+						</div>
+
+					</div>
 				</div>
 			</div>
+
 		</div>
 
 		<div className="largeSpacer-3">
